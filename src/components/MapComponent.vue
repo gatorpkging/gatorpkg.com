2b6b33885da26118dd0e494815c26d56d2dbe225
Tristan Chapman <tchap6507@gmail.com>
2025-06-11 01:25:51 -0400

    modified:   package-lock.json 	modified:   package.json 	new file:   src/components/MapComponent.vue 	modified:   src/index.js 	modified:   src/views/ContactView.vue

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default {
+  name: "MapComponent",
+};
+</script>
+
+<template>
+<div class="map">
+  <iframe width="100%" height="100%" loading="lazy" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJedTguHazPIgRXmetUgTxOPw&key=AIzaSyBKBx-EEgQSkno5JEzjkaTF92zwtpqIfEQ"></iframe>
+</div>
+</template>
+
+<style>
+</style>
