a9383bead043b6dd212226125008524b5fe8375d
Tristan Chapman <tchap6507@gmail.com>
2025-06-15 00:18:18 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/views/ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -54,7 +54,7 @@ const validateForm = () => {
 
 const sendEmail = async () => {
   try {
-    await emailjs.send(`${process.env.VITE_EMAILJS_SERVICE_ID}`, `${process.env.VITE_EMAILJS_TEMPLATE_ID}`,
+    await emailjs.send(`${import.meta.env.VITE_EMAILJS_SERVICE_ID}`, `${import.meta.env.VITE_EMAILJS_TEMPLATE_ID}`,
       {
         name: formData.value.name,
         email: formData.value.email,
@@ -62,7 +62,7 @@ const sendEmail = async () => {
         subject: formData.value.subject,
         message: formData.value.message
       },
-      `${process.env.VITE_EMAILJS_USER_ID}`
+      `${import.meta.env.VITE_EMAILJS_USER_ID}`
     );
     popupMessage.value = 'Your message has been sent successfully!';
     formData.value = { name: '', email: '', phone: '', subject: '', message: ''};
